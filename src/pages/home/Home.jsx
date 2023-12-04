4408d207615454bdb7d5ddeb4cd019f31ec0f467
Prince Gupta <prince.gupta@almond.solutions>
2023-12-04 13:03:41 +0530

    push

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Slider from '../../components/slider/Slider'
 import Category from '../../components/category/Category'
+import FeaturedProducts from '../../components/featuredProduct/FeaturedProducts';
+import "./home.css"
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider/>
       <Category/>
+      <FeaturedProducts/>
+
+      <div className='btnDiv'>
+       <button className="allProductBtn">
+       Show All Product
+       </button>
+      </div>
     </div>
   )
 }
